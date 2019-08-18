341afdc15d45156e72d77b4b27cffed343b55fbd
Juan Pablo <pablo-238@hotmail.com>
2019-08-18 14:17:41 -0500

    primer commit con migraciones

---
+++ database/migrations/2019_08_18_191247_create_vehiculo_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiculoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehiculo', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('dueno')->references('id')->on('dueno');
+            $table->string('placa');
+            $table->string('marca')->references('id')->on('marca');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehiculo');
+    }
+}
